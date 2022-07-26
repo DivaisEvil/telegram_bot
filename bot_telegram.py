b009632009f1be643eadb8fa40863affb7c13025
evildivais <evilduvais@gmail.com>
2022-07-27 01:16:56 +0300

    первая отправка, раьриает после 2го урока

--- bot_telegram.py
+++ bot_telegram.py
@@ -21,13 +21,32 @@ async def on_startup(_):
 #     except:
 #         await message.reply('Bot не добавлен, напишите ему')
 
+'''******************************КЛИЕНТСКАЯ ЧАСТЬ*******************************************'''
 @dp.message_handler(commands=['start', 'help'])
 async def command_start(message : types.Message):
 	try:
 		await bot.send_message(message.from_user.id, 'Приятного аппетита')
 		await message.delete()
 	except:
-		await message.reply('Общение с ботом через ЛС, напишите ем')
+		await message.reply('Общение с ботом через ЛС, напишите ему')
+
+@dp.message_handler(commands=['Режим_работы'])
+async def pizza_open_command(message : types.Message):
+	await bot.send_message(message.from_user.id, 'Вс-Чт с 9:00 до 20:00, Пт-Сб с 10:00 до 23:00')
+
+@dp.message_handler(commands=['Расположение'])
+async def pizza_place_command(message : types.Message):
+	await bot.send_message(message.from_user.id, 'ул. Колбасная 15')
+
+
+# @dp.message_handler(commands=['Меню'])
+# async def pizza_menu_command(message : types.Message):
+# 	for ret in cur.execute('SELECT * FROM menu').fetchall():
+# 	   await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nОписание: {ret[2]}\nЦена {ret[-1]}')
+'''*******************************АДМИНСКАЯ ЧАСТЬ*******************************************'''
+
+'''*********************************ОБЩАЯ ЧАСТЬ*********************************************'''
+
 
 # @dp.message_handler()
 # async def echo_send(message : types.Message):
