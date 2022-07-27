720bc4c39e949c1e9a8e7e8d17e915c8f2c2274f
evildivais <evilduvais@gmail.com>
2022-07-28 01:16:08 +0300

    первая отправка, раьриает после 5го урока

---
+++ keyboards/client_kb.py
@@ -0,0 +1,14 @@
+from  aiogram.types import ReplyKeyboardMarkup, KeyboardButton# ReplyKeyboardRemove
+
+b1 = KeyboardButton('/Режим_работы')
+b2 = KeyboardButton('/Расположение')
+b3 = KeyboardButton('/Меню')
+# b4 = KeyboardButton('/ю', request_contact=True)
+# b5 = KeyboardButton('/Ме', request_location=True)
+
+kb_client = ReplyKeyboardMarkup(resize_keyboard=True)#, one_time_keyboard=True)
+# resize_keyboard=True размеры кнопок пропорциональны
+# one_time_keyboard=True скрыть клаву после выбора
+
+kb_client.add(b1).row(b2).insert(b3).add(b4).insert(b5)
+# add все кнопки ро очереди row все в одну строку insert попытаться виестить если есть место
